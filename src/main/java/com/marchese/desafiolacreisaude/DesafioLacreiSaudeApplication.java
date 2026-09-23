36f0b87cbebe7b05de7125591a2330668707e784
Marchese04 <gabrielmarchesed.a@gmail.com>
2026-09-22 22:16:41 -0300

    First Commit

---
+++ src/main/java/com/marchese/desafiolacreisaude/DesafioLacreiSaudeApplication.java
@@ -0,0 +1,13 @@
+package com.marchese.desafiolacreisaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioLacreiSaudeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioLacreiSaudeApplication.class, args);
+    }
+
+}
